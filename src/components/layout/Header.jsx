63a2f8a534335d218794a41971b2c5771e702b6f
supertalent007 <cyberstar3666@gmail.com>
2024-11-08 11:27:28 +0200

    update mobile header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -46,66 +46,71 @@ const Header = () => {
         <>
             {
                 isMobile ?
-                    <header className="text-white px-4 py-8 flex items-center justify-between">
-                        <Link to="/">
-                            <img src="/logo.png" alt="Logo" className="w-32" />
-                        </Link>
-                        <div>
-                            <button
-                                onClick={toggleMenu}
-                                className={`block md:hidden text-white focus:outline-none ${isMenuOpen ? 'hidden' : ''}`}
-                            >
-                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
-                            </button>
-                            {
-                                isMenuOpen ?
-                                    <nav
-                                        ref={menuRef}
-                                        className={`${isMenuOpen
-                                            ? 'flex flex-col space-y-4 transform translate-x-0 ease-in-out duration-300 absolute ml-[-70px] mt-[-20px]'
-                                            : 'hidden transform translate-x-full'
-                                            } md:flex md:flex-row md:space-x-8 transition-transform bg-gray-500 z-[1000] py-2 rounded-md`}
-                                    >
-                                        <Link
-                                            to="/"
-                                            className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                    <header
+                        className={`px-4 py-4 fixed w-full max-w-[inherit] z-[500] bg-black transition-all duration-500 ${isScrolled ? 'shadow-[0_8px_6px_0_rgba(255,255,255,0.1)] rounded-lg top-[-5px]' : 'top-0'}`}
+                    >
+                        <div className="text-white container flex items-center justify-between max-w-[inherit]">
+
+                            <Link to="/">
+                                <img src="/logo.png" alt="Logo" className="w-32" />
+                            </Link>
+                            <div>
+                                <button
+                                    onClick={toggleMenu}
+                                    className={`block md:hidden text-white focus:outline-none ${isMenuOpen ? 'hidden' : ''}`}
+                                >
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    </svg>
+                                </button>
+                                {
+                                    isMenuOpen ?
+                                        <nav
+                                            ref={menuRef}
+                                            className={`${isMenuOpen
+                                                ? 'flex flex-col space-y-4 transform translate-x-0 ease-in-out duration-300 absolute ml-[-70px] mt-[-20px]'
+                                                : 'hidden transform translate-x-full'
+                                                } md:flex md:flex-row md:space-x-8 transition-transform bg-gray-500 z-[1000] py-2 rounded-md`}
                                         >
-                                            Home
-                                        </Link>
-                                        <Link
-                                            to="/about"
-                                            className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
-                                        >
-                                            About
-                                        </Link>
-                                        <Link
-                                            to="/pricing"
-                                            className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
-                                        >
-                                            Pricing
-                                        </Link>
-                                        <Link
-                                            to="/docs"
-                                            className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
-                                        >
-                                            Docs
-                                        </Link>
-                                        <Link
-                                            to="/faq"
-                                            className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
-                                        >
-                                            FAQ
-                                        </Link>
-                                        <Link to="/login" className="block px-5 hover:text-gray-400">Login</Link>
-                                        <Link to="/register" className="block px-5 hover:text-gray-400">Register</Link>
-                                    </nav> : ''
-                            }
+                                            <Link
+                                                to="/"
+                                                className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                            >
+                                                Home
+                                            </Link>
+                                            <Link
+                                                to="/about"
+                                                className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                            >
+                                                About
+                                            </Link>
+                                            <Link
+                                                to="/pricing"
+                                                className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                            >
+                                                Pricing
+                                            </Link>
+                                            <Link
+                                                to="/docs"
+                                                className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                            >
+                                                Docs
+                                            </Link>
+                                            <Link
+                                                to="/faq"
+                                                className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                            >
+                                                FAQ
+                                            </Link>
+                                            <Link to="/login" className="block px-5 hover:text-gray-400">Login</Link>
+                                            <Link to="/register" className="block px-5 hover:text-gray-400">Register</Link>
+                                        </nav> : ''
+                                }
+                            </div>
                         </div>
                     </header> :
                     <header
-                        className={`px-4 py-8 fixed top-0 w-full max-w-[inherit] z-[500] bg-black ${isScrolled ? 'shadow-[0_8px_6px_0_rgba(255,255,255,0.1)] rounded-lg' : ''
+                        className={`px-4 py-8 fixed top-0 w-full max-w-[inherit] z-[500] bg-black transition-all duration-500 ${isScrolled ? 'shadow-[0_8px_6px_0_rgba(255,255,255,0.1)] rounded-lg' : ''
                             }`}
                     >
                         <div className="text-white container flex items-center justify-between max-w-[inherit]">
