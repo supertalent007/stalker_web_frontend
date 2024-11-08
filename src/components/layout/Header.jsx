fb853b647838820664b387e96f1a07eb25198594
supertalent007 <cyberstar3666@gmail.com>
2024-11-08 11:23:18 +0200

    update header style

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
     const menuRef = useRef(null);
 
     const toggleMenu = () => {
@@ -21,15 +22,23 @@ const Header = () => {
     useEffect(() => {
         checkMobileScreen();
         const handleClickOutside = (event) => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+
             if (menuRef.current && !menuRef.current.contains(event.target)) {
                 setIsMenuOpen(false);
             }
         };
 
         document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener("scroll", handleClickOutside);
 
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener("scroll", handleClickOutside);
         };
     }, []);
 
@@ -37,7 +46,7 @@ const Header = () => {
         <>
             {
                 isMobile ?
-                    <header className="text-white px-4 pt-8 flex items-center justify-between">
+                    <header className="text-white px-4 py-8 flex items-center justify-between">
                         <Link to="/">
                             <img src="/logo.png" alt="Logo" className="w-32" />
                         </Link>
@@ -95,45 +104,74 @@ const Header = () => {
                             }
                         </div>
                     </header> :
-                    <header className="text-white px-4 pt-8 flex items-center justify-between">
-                        <Link to="/">
-                            <img src="/logo.png" alt="Logo" className="w-32" />
-                        </Link>
-                        <button
-                            onClick={toggleMenu}
-                            className="block md:hidden text-white focus:outline-none"
-                        >
-                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                        </button>
+                    <header
+                        className={`px-4 py-8 fixed top-0 w-full max-w-[inherit] z-[500] bg-black ${isScrolled ? 'shadow-[0_8px_6px_0_rgba(255,255,255,0.1)] rounded-lg' : ''
+                            }`}
+                    >
+                        <div className="text-white container flex items-center justify-between max-w-[inherit]">
+                            <Link to="/">
+                                <img src="/logo.png" alt="Logo" className="w-32" />
+                            </Link>
+                            <button
+                                onClick={() => { }}
+                                className="block md:hidden text-white focus:outline-none"
+                            >
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            </button>
 
-                        <nav className='md:flex space-x-4 md:space-x-[40px] z-[1000]'>
-                            <div className='md:flex md:flex-row md:space-x-4'>
-                                <Link to="/" className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0">
-                                    Home
-                                </Link>
-                                <Link to="/about" className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0">
-                                    About
-                                </Link>
-                                <Link to="/pricing" className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0">
-                                    Pricing
-                                </Link>
-                                <Link to="/docs" className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0">
-                                    Docs
-                                </Link>
-                                <Link to="/faq" className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0">
-                                    FAQ
-                                </Link>
-                            </div>
-                        </nav>
+                            <nav className="md:flex space-x-4 md:space-x-[40px] z-[1000]">
+                                <div className="md:flex md:flex-row md:space-x-4">
+                                    <Link
+                                        to="/"
+                                        className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                    >
+                                        Home
+                                    </Link>
+                                    <Link
+                                        to="/about"
+                                        className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                    >
+                                        About
+                                    </Link>
+                                    <Link
+                                        to="/pricing"
+                                        className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                    >
+                                        Pricing
+                                    </Link>
+                                    <Link
+                                        to="/docs"
+                                        className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                    >
+                                        Docs
+                                    </Link>
+                                    <Link
+                                        to="/faq"
+                                        className="block px-5 relative after:block after:content-[''] after:border-b-2 after:border-gray-400 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:right-0 after:bottom-[-2px] after:left-0"
+                                    >
+                                        FAQ
+                                    </Link>
+                                </div>
+                            </nav>
 
-                        <nav className='md:flex space-x-4 md:space-x-[40px]'>
-                            <div className='md:flex md:flex-row md:space-x-4'>
-                                <Link to="/login" className="block px-5 hover:text-gray-400">Login</Link>
-                                <Link to="/register" className="block px-5 hover:text-gray-400">Register</Link>
-                            </div>
-                        </nav>
+                            <nav className="md:flex space-x-4 md:space-x-[40px]">
+                                <div className="md:flex md:flex-row md:space-x-4">
+                                    <Link to="/login" className="block px-5 hover:text-gray-400">
+                                        Login
+                                    </Link>
+                                    <Link to="/register" className="block px-5 hover:text-gray-400">
+                                        Register
+                                    </Link>
+                                </div>
+                            </nav>
+                        </div>
                     </header>
             }
         </>
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSubscriptionPlan } from '../../utils/Constants';
+import SubscriptionPlanCard from '../../components/cards/SubscriptionPlanCard';
 
 const Register = () => {
+    const [subscription, setSubscription] = useState(null);
+    const [subscriptions, setSubscriptions] = useState([]);
+    const [plan, setPlan] = useState(1);
+
+    useEffect(() => {
+        const filters = [
+            { membership: "standard", plan: plan },
+            { membership: "gold", plan: plan },
+            { membership: "platinum", plan: plan },
+        ];
+
+        setSubscriptions(getSubscriptionPlan(filters));
+    }, [plan]);
+
     return (
-        <div className="flex flex-col items-center justify-center w-full text-center">
-            <h1 className="font-[russo] text-[50px] text-primary">Standerd Plan / Register</h1>
-            <div className='grid grid-cols-2 w-[70%]'>
-                <RegisterForm />
+        <div className="flex flex-col items-center justify-center w-full text-primary">
+            <h1 className="font-[russo] text-[50px] text-primary text-center">Choose Your Plan</h1>
+            <div className='grid grid-cols-3 mb-5 gap-5'>
+                <button
+                    className='py-5 px-[80px] rounded-[10px] font-[600] bg-[#222] hover:bg-primary hover:text-black transition-all duration-500'
+                    onClick={e => setPlan(1)}
+                >
+                    Monthly
+                </button>
+                <button
+                    className='py-5 px-[80px] rounded-[10px] font-[600] bg-[#222] hover:bg-primary hover:text-black transition-all duration-500'
+                    onClick={e => setPlan(2)}
+                >
+                    6 Months
+                </button>
+                <button
+                    className='py-5 px-[80px] rounded-[10px] font-[600] bg-[#222] hover:bg-primary hover:text-black transition-all duration-500'
+                    onClick={e => setPlan(3)}
+                >
+                    Yearly
+                </button>
+            </div>
+            <div className='grid grid-cols-3 w-full'>
+                {
+                    subscriptions.map((subscriptionData, index) => {
+                        return (
+                            <SubscriptionPlanCard subscriptionData={subscriptionData} key={`subscription_${index}`} buttonName='Subscribe' />
+                        )
+                    })
+                }
+                {/* <RegisterForm /> */}
             </div>
         </div>
     );
